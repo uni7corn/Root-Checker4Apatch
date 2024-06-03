1a9be692ea2e412695195562ca78bee7b7b53509
hackest <25418711@qq.com>
2024-06-03 23:20:54 +0800

    修改最低sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vineelsai.rootchecker"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 2
         versionName = "1.0.1"
